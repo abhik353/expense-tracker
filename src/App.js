f692831df295432c3bb9d17e957932273d6c9125
Abhinav <abhinavk353.ak@gmail.com>
2020-11-09 21:03:25 +0530

    contextApi initialized

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import Balance from './components/Balance';
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
 import AddTransaction from './components/AddTransaction';
+import {GlobalProvider} from './context/GlobalState'
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
         <Header/>
         <Balance/>
         <IncomeExpenses/>
         <TransactionList/>
         <AddTransaction/>
-    </div>
+    </GlobalProvider>
   );
 }
 
